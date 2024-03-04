433234f014b3a172ea39138f17b66c8a615d70da
Samuel Bradley <samuel@samuel-bradley.com>
2024-03-04 09:29:19 +0000

    Use Link components instead of "a" tags

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import Navigation from "../navigation";
 
 export default function About() {
@@ -10,17 +11,17 @@ export default function About() {
       <div className="max-w-3xl">
         
         <h2>About the memorial</h2>
-        <p>In <a href="https://www.banburystmary.org.uk/">St Mary's Church</a>, on Horse Fair, Banbury, there is a
+        <p>In <Link href="https://www.banburystmary.org.uk/">St Mary's Church</Link>, on Horse Fair, Banbury, there is a
             memorial listing the names of the town's men who died in the Second World War. This memorial consists of
             three panels: one central panel containing 78 names, which dates from just after the war, and two further
             panels to the left and right which were added in 2003:</p>
         <div className="items-center mb-4">
-            <a href="memorial.jpg" target="_blank" title="Photograph of memorial panels (opens in new tab)">
+            <Link href="memorial.jpg" target="_blank" title="Photograph of memorial panels (opens in new tab)">
                 <Image src="/memorial_small.jpg" alt="Photograph of memorial panels" width={600} height={358} className="border rounded mx-auto"/>
-            </a>
+            </Link>
         </div>
-        <p>Mr. Harry Cowie, war veteran and vice president of the <a href="http://counties.britishlegion.org.uk/branches/banbury">
-            Banbury branch of the Royal British Legion</a>, spent years researching other names which should have been
+        <p>Mr. Harry Cowie, war veteran and vice president of the <Link href="http://counties.britishlegion.org.uk/branches/banbury">
+            Banbury branch of the Royal British Legion</Link>, spent years researching other names which should have been
             on the original memorial, but were not. Thanks to his work, the two further panels with an additional 34 and
             22 names were added to the left and right of the original memorial.</p>
         <p>The men listed on the memorial include the following:</p>
@@ -37,25 +38,25 @@ export default function About() {
             <li>1 who was in the Life Guards and</li>
             <li>1 who was in the Royal Australian Air Force</li>
         </ul>
-        <p>The youngest man on the memorial was 17 (<a href="/memorial/Williams%2C%20S%20J%20Q">Stuart John Quare
-            Williams</a>, a Cadet in the Merchant Navy) and the oldest was 61 (<a href="/memorial/Parker%2C%20R%20W">
-            Richard Walter Parker</a>, civilian).</p>
-        <p>The earliest date of death is 20/12/1939 (<a href="/memorial/Selby%2C%20D%20C">Donald Cranston Selby</a>,
-            killed at a railway crossing) and the latest is 17/10/1956 (<a href="/memorial/O%20Carroll%2C%20M">Michael
-            O'Carroll</a>, accidentally shot while on guard duty in Cyprus).</p>
-        <p>To date, 3 men remain unidentified (<a href="/memorial/Harrison%2C%20A%20R">A R Harrison</a>, <a href="/memorial/Taylor%2C%20E">
-            E Taylor</a> and <a href="/memorial/Booth%2C%20S">S Booth</a>).</p>
+        <p>The youngest man on the memorial was 17 (<Link href="/memorial/Williams%2C%20S%20J%20Q">Stuart John Quare
+            Williams</Link>, a Cadet in the Merchant Navy) and the oldest was 61 (<Link href="/memorial/Parker%2C%20R%20W">
+            Richard Walter Parker</Link>, civilian).</p>
+        <p>The earliest date of death is 20/12/1939 (<Link href="/memorial/Selby%2C%20D%20C">Donald Cranston Selby</Link>,
+            killed at a railway crossing) and the latest is 17/10/1956 (<Link href="/memorial/O%20Carroll%2C%20M">Michael
+            O'Carroll</Link>, accidentally shot while on guard duty in Cyprus).</p>
+        <p>To date, 3 men remain unidentified (<Link href="/memorial/Harrison%2C%20A%20R">A R Harrison</Link>, <Link href="/memorial/Taylor%2C%20E">
+            E Taylor</Link> and <Link href="/memorial/Booth%2C%20S">S Booth</Link>).</p>
 
         <h2 className="text-2xl">About this site</h2>
         <p>These pages are dedicated to the men of Banbury who gave their lives in the Second World War. Their names and
-            details are listed on the <a href="/">Memorial</a> page, and there is further information about our town
-            during the war on the <a href="/wartime-banbury">Wartime Banbury</a> page.</p>
+            details are listed on the <Link href="/">Memorial</Link> page, and there is further information about our town
+            during the war on the <Link href="/wartime-banbury">Wartime Banbury</Link> page.</p>
         <p>We have tried to identify all of the men listed on the memorial. The site lists all of the names on the three
-            panels, and we have done further research using the <a href="https://www.cwgc.org/">Commonwealth War Graves
-            Commission</a> and <a href="https://www.freebmd.org.uk/"> FreeBMD</a>, as well as information from documents
-            and news clippings held by <a href="https://www.oxfordshire.gov.uk/residents/leisure-and-culture/libraries/find-library/banbury-library">
-            Banbury Library</a>. We have also taken photographs of some graves. We have been as careful as we can with
-            our research, but if we have made any mistakes, we hope you will <a href="/contact">let us know</a> so we
+            panels, and we have done further research using the <Link href="https://www.cwgc.org/">Commonwealth War Graves
+            Commission</Link> and <Link href="https://www.freebmd.org.uk/"> FreeBMD</Link>, as well as information from documents
+            and news clippings held by <Link href="https://www.oxfordshire.gov.uk/residents/leisure-and-culture/libraries/find-library/banbury-library">
+            Banbury Library</Link>. We have also taken photographs of some graves. We have been as careful as we can with
+            our research, but if we have made any mistakes, we hope you will <Link href="/contact">let us know</Link> so we
             can correct them.</p>
         <p>Hundreds of men from the Banbury area fought in the war; in the British Army, the Royal and Merchant Navies
             and in the Royal Air Force. Some were sent to Europe, to North Africa, the Middle East and the Far East,
--- app/navigation.tsx
+++ app/navigation.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Navigation({currentPage}: {currentPage: string}) {
   return (
     <nav role="nav" className="flex flex-row gap-x-3">
-      <a href="/" className="hover:underline" aria-current={currentPage == "memorial" ? "page" : undefined}>Memorial</a>
-      <a href="/about" className="hover:underline" aria-current={currentPage == "about" ? "page" : undefined}>About</a>
-      <a href="/wartime-banbury" className="hover:underline" aria-current={currentPage == "wartime-banbury" ? "page" : undefined}>Wartime Banbury</a>
-      <a href="/contact" className="hover:underline" aria-current={currentPage == "contact" ? "page" : undefined}>Contact</a>
+      <Link href="/" className="hover:underline" aria-current={currentPage == "memorial" ? "page" : undefined}>Memorial</Link>
+      <Link href="/about" className="hover:underline" aria-current={currentPage == "about" ? "page" : undefined}>About</Link>
+      <Link href="/wartime-banbury" className="hover:underline" aria-current={currentPage == "wartime-banbury" ? "page" : undefined}>Wartime Banbury</Link>
+      <Link href="/contact" className="hover:underline" aria-current={currentPage == "contact" ? "page" : undefined}>Contact</Link>
     </nav>
   );
 }
