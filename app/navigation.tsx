d669ed4c1b6d030ce00bd1ebb6749aff2627bbc6
Samuel Bradley <samuel@samuel-bradley.com>
2024-03-04 12:45:41 +0000

    Remove unnecessary nav role

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navigation({currentPage}: {currentPage: string}) {
   return (
-    <nav role="nav" className="flex flex-row gap-x-3">
+    <nav className="flex flex-row gap-x-3">
       <Link href="/" className="hover:underline" aria-current={currentPage == "memorial" ? "page" : undefined}>Memorial</Link>
       <Link href="/about" className="hover:underline" aria-current={currentPage == "about" ? "page" : undefined}>About</Link>
       <Link href="/wartime-banbury" className="hover:underline" aria-current={currentPage == "wartime-banbury" ? "page" : undefined}>Wartime Banbury</Link>
