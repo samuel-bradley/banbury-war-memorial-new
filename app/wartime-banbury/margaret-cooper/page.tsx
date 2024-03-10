752df4a73a13c8ba57703db6dd07bd04c8c7a9fe
Samuel Bradley <samuel@samuel-bradley.com>
2024-03-10 23:51:19 +0000

    Add Margaret Cooper page

---
+++ app/wartime-banbury/margaret-cooper/page.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import Main from "../../main";
+
+export default function MargaretCooper() {
+  return (
+    <Main pageName="margaret-cooper" heading="Wartime Banbury" content = {
+      <>
+        <h2>Margaret Cooper</h2>
+        <p>Margaret Cooper (nee McKeevor) was born in Banbury in 1924. She was one of three children of William and Ada
+          McKeevor - she had an older brother, William, and an older sister, Mary.</p>
+        <p>Their grandfather, also William, had come over from Ireland and built a house in the Grimsbury area of the
+          town (now McKeevor Close). It had seven bedrooms and an acre of ground in which were several cottages. In
+          later years the house was demolished and the land sold to the council, who built new houses there.</p>
+        <p>Margaret attended St John's Priory School in Banbury, next to the Catholic Church, where she would eventually
+          be married. She left school at 14 and completed a secretarial course, after which she took a job at the Milk
+          Marketing Board in Cherwell Street. She was there in 1939 when war broke out and remembers the whole office
+          falling silent as they listened to the announcement on the radio. Not long afterwards, Margaret stood and
+          watched the women on parade in the ATS outside the Town Hall and told herself that as soon as she was eighteen
+          she would join the WAAF.</p>
+        <p>Life in Banbury was not drastically changed during the war. Clothes and food were in short supply for most,
+          but the McKeevor family were lucky in that Margaret's brother and some of their other relatives were farmers
+          (a reserved occupation) and from them they received rabbits, pheasants, partridges and more. They also kept
+          chickens and had a big vegetable garden which provided them with fruit and vegetables.</p>
+        <p>Every family who was able, had to take in evacuees from the London area and the McKeevors were allocated two
+          very nice Jewish boys aged 11 or 12 years, from Bow Central School. Margaret remembers their parents, who had
+          some sort of fabrics business, coming down to visit them and bringing several yards of tweed material which
+          Margaret and her sister had made up by a local tailor.</p>
+        <p>Miss Brown's cake shop in Parson's Street sold traditional Banbury Cakes and Margaret and her sister would
+          arrange for parcels of these to be sent abroad to their boyfriends serving overseas.</p>
+        <p>The social life during the war was good, with dances held in Banbury Town Hall and surrounding villages. The
+          McKeevors had a canteen in their Church Hall which served aircrew and servicemen from RAF Upper Heyford and
+          Bicester OTU. They only had beetroot to fill the sandwiches but the air crews did not complain.</p>
+        <p>Although Banbury was only bombed directly twice during the war, Margaret remembers the terror of hearing
+          German bombers flying over the town towards Coventry. The noise of the aircraft was deafening and the sky was
+          lit up; there were no air raid shelters so her father pushed them all under the dining-room table for safety.</p>
+        <p>Margaret's sister, Mary, had gone to work in an ammunitions factory, but when Margaret turned eighteen she
+          stuck to her decision to join the WAAF, the Women's Auxiliary Air Force. Although WAAFs did not participate in
+          active combat, they were exposed to the same dangers as any personnel working on the Home Front, and many
+          WAAFs lost their lives during the war. Initially, Margaret worked at Bletchley Park on the Enigma Code; she
+          recalls that being stationed there was worse than being in prison. In 1943, she transferred to Chipping Warden
+          air base, home to 12 OTU (Operational Training Unit).</p>
+        <p>Margaret worked as a Teleprinter Operator in the Signals Office on a 24-hour watch consisting of three
+          eight-hour shifts. Her work involved receiving and sending out Top Secret messages, and sending telegrams to
+          the next of kin advising of missing aircrew. It was also the job of the WAAFs on duty in the Signals Office to
+          serve hot coffee and rum to air crews returning from night flights.</p>
+        <p>Although 12 OTU was a training unit, bomber crews there were also involved in "Nickel" operations (leaflet
+          drops), as well as training missions, and there were several aircraft lost or missing. Some crashed locally,
+          and many of the deceased airmen were buried in the Southam Road cemetery in Banbury. In 1943 alone there were
+          12 crashes involving aircraft from 12 OTU. Margaret remembers one particular crash well; on the 30th April
+          1944, a Wellington bomber ran out of fuel and landed on the airfield split in two halves. There were no
+          fatalities on this occasion but the Air Gunner was seriously injured and W/O Kenneth W. Cooper escaped with a
+          broken knee joint. A few months later, Kenneth and Margaret were married at St John's Catholic Church in
+          Banbury.</p>
+        <p>In the years after the war, Margaret worked as a secretary until retirement, at which time she qualified as a
+          dance teacher. At the time of writing, she still dances once or twice a week, despite being 86. She lives in
+          Bognor Regis now, but still returns to Banbury to visit her sister who lives in Bloxham.</p>
+        <p className="mt-6 text-center"><Link href="/wartime-banbury">Back to Wartime Banbury</Link></p>
+      </>
+    }/>
+  )
+}
