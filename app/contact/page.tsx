e1e7011586304bd8405a4894e4b4ef7984b2770d
Samuel Bradley <samuel@samuel-bradley.com>
2024-03-13 09:20:49 +0000

    Tidy up contact code

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -34,10 +34,9 @@ export default function Contact() {
       ReplyToAddresses: [rawFormData.from]
     };
     
-    var sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+    new AWS.SES({ apiVersion: "2010-12-01" })
       .sendEmail(params)
-      .promise();
-    sendPromise
+      .promise()
       .then(function (data: { MessageId: any; }) {
         console.log(data.MessageId);
       })
@@ -45,6 +44,7 @@ export default function Contact() {
         console.error(err, err.stack);
       });
   }
+
   return (
     <Main pageName="contact" heading="Contact" content = {
       <>
@@ -53,29 +53,30 @@ export default function Contact() {
           <div className="border-b border-gray-900/10 pb-6">
             <div className="grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
               <div className="sm:col-span-6">
-                <p className="max-w-sm">If you have any questions or corrections, please leave a message and we'll try to get back to you soon.</p>
+                <p className="max-w-sm">If you have any questions or corrections, please leave a message and we'll try
+                  to get back to you soon.</p>
               </div>
               <div className="sm:col-span-6">
-                  <label htmlFor="from" className="block leading-6 mb-1">Your email address:</label>
-                  <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
-                    <input type="email" id="from" name="from" className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500" placeholder="you@somewhere.com"/>
-                  </div>
+                <label htmlFor="from" className="block leading-6 mb-1">Your email address:</label>
+                <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
+                  <input type="email" id="from" name="from" className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500" placeholder="you@somewhere.com"/>
+                </div>
               </div>
               <div className="sm:col-span-6">
-                  <label htmlFor="from" className="block leading-6 mb-1">Subject:</label>
-                  <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
-                  <input type="text" id="subject" name="subject" className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500" placeholder="What's this about?"/>
-                  </div>
+                <label htmlFor="from" className="block leading-6 mb-1">Subject:</label>
+                <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
+                <input type="text" id="subject" name="subject" className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500" placeholder="What's this about?"/>
+                </div>
               </div>
               <div className="sm:col-span-6">
-                  <label htmlFor="from" className="block leading-6 mb-1">Message:</label>
-                  <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
-                    <textarea id="message" name="message" cols={50} rows={5} className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500" placeholder="What would you like to say?"/>
-                  </div>
+                <label htmlFor="from" className="block leading-6 mb-1">Message:</label>
+                <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
+                  <textarea id="message" name="message" cols={50} rows={5} className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500" placeholder="What would you like to say?"/>
+                </div>
               </div>
             </div>
           </div>
-          <button type="submit" className="rounded-md bg-gray-500 px-3 py-2 font-semibold text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send</button>
+          <button type="submit" className="rounded-md bg-gray-500 px-3 py-2 font-semibold text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">Send</button>
         </form>
       </>
     }/>
