2d85c5fc12ec91d8ea7e48cb9af5ce36ddd7c1b2
Samuel Bradley <samuel@samuel-bradley.com>
2024-04-27 20:31:44 +0100

    Add remaining form fields to admin page

---
+++ app/admin/records/[nameInUrl]/input.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+interface InputProps {
+  type: 'text'|'number'|'date'
+  label: string
+  nameAndId: string
+  value: string|number|undefined
+  placeholder: string|undefined
+  required: boolean
+}
+
+export function Input(props: InputProps) {
+  return (
+    <div className="sm:col-span-10">
+      <label htmlFor={props.nameAndId} className="block leading-6 mb-1">{props.label}</label>
+      <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
+        <input type={props.type} required={props.required} id={props.nameAndId} name={props.nameAndId} defaultValue={props.value} placeholder={props.placeholder}
+          className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500"/>
+      </div>
+    </div>
+  )
+}
+
+interface SelectProps {
+  label: string
+  nameAndId: string
+  options: { label: string; value: string }[];
+  selectedValue: string|undefined
+  required: boolean
+}
+
+export function Select(props: SelectProps) {
+  return (
+    <div className="sm:col-span-10">
+      <label htmlFor={props.nameAndId} className="block leading-6 mb-1">{props.label}</label>
+      <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
+        <select required={props.required} id={props.nameAndId} name={props.nameAndId} defaultValue={props.selectedValue}
+          className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500">
+            {props.options.map(option => <option key={option.value} value={option.value}>{option.label}</option>)}
+        </select>
+      </div>
+    </div>
+  )
+}
--- app/admin/records/[nameInUrl]/page.tsx
+++ app/admin/records/[nameInUrl]/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { FormEvent, useEffect, useState, useTransition } from "react";
 import { MemorialRecord } from "../../../dynamoDb";
 import SubmitButton from "@/app/submit-button";
+import { Input, Select } from "./input";
 
 export default function Page({ params }: { params: { nameInUrl: string } }) {
   const [record, setData] = useState<MemorialRecord>()
@@ -46,8 +47,15 @@ export default function Page({ params }: { params: { nameInUrl: string } }) {
     })
   }
   
+const memorialPanelOptions = [
+  { value: 'none', label: 'none' },
+  { value: 'left', label: 'left' },
+  { value: 'centre', label: 'centre' },
+  { value: 'right', label: 'right' }
+]
+
   return (
-    <Main pageName="records" heading={params.nameInUrl} content = {
+    <Main pageName={params.nameInUrl} heading={params.nameInUrl} content = {
       <>
         { isLoading && (<p>Loading...</p>) }
         { errorMessage && (<p>{errorMessage}</p>) }
@@ -56,13 +64,23 @@ export default function Page({ params }: { params: { nameInUrl: string } }) {
           <form onSubmit={onSave}>
             <input type="hidden" name="nameInUrl" value={record?.nameInUrl}/>
             <div className="border-b border-gray-900/10 pb-6">
-              <div className="grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
-                <div className="sm:col-span-6">
-                  <label htmlFor="nameOnMemorial" className="block leading-6 mb-1">Name on memorial:</label>
-                  <div className="flex rounded-md shadow-sm ring-2 ring-inset ring-gray-500 focus-within:ring-inset focus-within:ring-gray-400 sm:max-w-md">
-                    <input type="text" required id="nameOnMemorial" name="nameOnMemorial" defaultValue={record?.nameOnMemorial} className="block flex-1 border-0 bg-transparent py-1.5 pl-2 focus:ring-0 sm:leading-6 placeholder:text-gray-500" placeholder="Surname, A B"/>
-                  </div>
-                </div>
+              <div className="grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-10">
+                <Input type="text" label="Name on memorial:" nameAndId="nameOnMemorial" value={record?.nameOnMemorial} placeholder="Surname, A B" required={true}/>
+                <Select label="Memorial panel:" nameAndId="memorialPanel" options={memorialPanelOptions} selectedValue={record?.memorialPanel} required={true}/>
+                <Input type="text" label="Full name:" nameAndId="fullName" value={record?.fullName} placeholder="John Smith" required={true}/>
+                <Input type="text" label="Rank:" nameAndId="rank" value={record?.rank} placeholder="Captain" required={true}/>
+                <Input type="text" label="Service details:" nameAndId="serviceDetails" value={record?.serviceDetails} placeholder="Service number and branch" required={true}/>
+                <Input type="number" label="Age at death:" nameAndId="ageAtDeath" value={record?.ageAtDeath} placeholder="42" required={true}/>
+                <Input type="date" label="Date of death:" nameAndId="dateOfDeath" value={record?.dateOfDeath && new Date(record?.dateOfDeath).toISOString().split('T')[0]} placeholder="" required={true}/>
+                <Input type="text" label="Place of birth:" nameAndId="placeOfBirth" value={record?.placeOfBirth} placeholder="New York, USA" required={true}/>
+                <Input type="text" label="Parents:" nameAndId="parents" value={record?.parents} placeholder="John Smith and Jane Smith" required={true}/>
+                <Input type="text" label="Mother's maiden name:" nameAndId="motherMaidenName" value={record?.motherMaidenName} placeholder="Jones" required={true}/>
+                <Input type="text" label="Parents' marriage details:" nameAndId="parentsMarriageDetails" value={record?.parentsMarriageDetails} placeholder="Date and place of marriage" required={true}/>
+                <Input type="text" label="Wife:" nameAndId="wife" value={record?.wife} placeholder="Mary Smith" required={true}/>
+                <Input type="text" label="Wife's maiden name:" nameAndId="wifeMaidenName" value={record?.wifeMaidenName} placeholder="Johnson" required={true}/>
+                <Input type="text" label="Marriage details:" nameAndId="marriageDetails" value={record?.marriageDetails} placeholder="Date and place of marriage" required={true}/>
+                <Input type="text" label="Cemetery:" nameAndId="cemetery" value={record?.cemetery} placeholder="XYZ Cemetery" required={true}/>
+                <Input type="text" label="Additional information:" nameAndId="additionalInfo" value={record?.additionalInfo} placeholder="Any other information" required={true}/>
               </div>
             </div>
             <div className="flex justify-center">
