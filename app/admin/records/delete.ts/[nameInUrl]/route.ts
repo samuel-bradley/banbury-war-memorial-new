877a4b623e3a7f28b3792b2a72f4363bce0705a8
Samuel Bradley <samuel@samuel-bradley.com>
2024-05-15 09:38:53 +0100

    Update route to redirect on success and use POST

--- app/admin/records/delete.ts/[nameInUrl]/route.ts
+++ app/admin/records/delete.ts/[nameInUrl]/route.ts
@@ -2,11 +2,13 @@
 
 import { deleteRecord } from "@/app/dynamoDb";
 
-export async function GET(_: Request, { params }: { params: { nameInUrl: string } }) {
+export async function POST(_: Request, { params }: { params: { nameInUrl: string } }) {
   try {
-    const deleted: boolean = await deleteRecord(params.nameInUrl);
-    const json = JSON.stringify(deleted)
-    return Response.json(json)
+    if (await deleteRecord(params.nameInUrl)) {
+      return Response.redirect(new URL('/admin/records'))
+    } else {
+      return Response.json(false)
+    }
   } catch (error) {
     console.error('Error deleting record:', error);
     return Response.error
