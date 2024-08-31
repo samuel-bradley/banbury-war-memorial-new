7cf5824002a3f4ffa0fa34bde70339244f723f34
Samuel Bradley <samuel@samuel-bradley.com>
2024-08-31 09:54:56 +0100

    Redirect to /memorial from / to maintain existing link structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-import Link from 'next/link'
-import { MemorialRecord, listRecords } from './dynamoDb'
-import Main from './main'
+import { redirect } from 'next/navigation';
 
-export default async function Memorial() {
-  const records = listRecords()
-  return (
-    <Main pageName="memorial" heading="Memorial" content = {
-      <>
-        <p className="italic mb-6">
-          They shall not grow old as we who are left grow old<br/>
-          Age shall not weary them, nor the years condemn<br/>
-          At the going down of the sun and in the morning<br/>
-          We will remember them.
-        </p>
-        <ul className="flex flex-col gap-3 items-center">{
-          (await records).map((record: MemorialRecord) => {
-            return <li key={record.nameInUrl}><Link href={`/memorial/${record.nameInUrl}`} className="no-underline">{record.nameOnMemorial}</Link></li>
-          })
-        }</ul>
-      </>
-    }/>
-  )
+export default function Page() {
+  redirect('/memorial');
 }
