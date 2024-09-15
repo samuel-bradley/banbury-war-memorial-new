a3679d9aa3ff5856fb1a99cfaed2d7a3c9305cc2
Samuel Bradley <samuel@samuel-bradley.com>
2024-09-15 16:43:13 +0100

    Set page titles according to headings

--- app/main.tsx
+++ app/main.tsx
@@ -5,6 +5,7 @@ export default function Main(
   ) {
   return (
     <main className="min-h-screen flex flex-col gap-y-5 items-center p-24 font-sans">
+      <title>{`Banbury War Memorial - ${heading}`}</title>
       <Navigation currentPage={pageName}/>
         <h1 className="text-4xl">{heading}</h1>
         <div className="max-w-3xl">{content}</div>
